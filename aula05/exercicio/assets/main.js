6169b6b81e8303b9c749105c23c0cf30c2200c56
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-22 01:28:12 -0300

    lesson 5 exercise conclusion.

---
+++ aula05/exercicio/assets/main.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const formData = new FormData(form)
+  const formValues = Object.fromEntries(formData.entries())
+
+  console.log(formValues)
+})
+
