e3d95168e85b0512d106b9412fc79614243c1836
Flávio Mattos <80709540+FlavioMattosDev@users.noreply.github.com>
2023-08-17 16:04:14 -0300

    Update getTaskById.js

--- modulo-02-logica-de-programacao/projeto/execucao/assets/js/getTaskById.js
+++ modulo-02-logica-de-programacao/projeto/execucao/assets/js/getTaskById.js
@@ -1,3 +1,15 @@
-import { tasks } from "./in-memory.js";
+import { tasks } from "./in-memory";
 
-export const getTaskById = () => {};
+export const getTaskById = (taskId) => {
+	try {
+		const task = tasks.find((task) => task.id === taskId);
+
+		if (!task) {
+			throw new Error("Tarefa não encontrada!");
+		}
+
+		return task;
+	} catch (error) {
+		console.log(error.message);
+	}
+};
