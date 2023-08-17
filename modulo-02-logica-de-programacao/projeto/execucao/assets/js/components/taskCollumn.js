0e73ebcdbb9e7bbf7fb4e42b04f353b19b25cd07
Anderson Junior <acfaj2010@gmail.com>
2023-08-17 16:44:26 -0300

    Front end: tela trello like componentizada e modal de cadastro

---
+++ modulo-02-logica-de-programacao/projeto/execucao/assets/js/components/taskCollumn.js
@@ -0,0 +1,74 @@
+class TaskCollumn extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.style());
+  }
+
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "tasksCollumn");
+
+    const name = this.getAttribute("name");
+
+    const collumnName = document.createElement("p");
+    collumnName.setAttribute("class", "collumnName");
+    collumnName.innerText = name;
+
+    const collumnContent = document.createElement("div");
+    collumnContent.innerHTML = this.innerHTML;
+
+    componentRoot.appendChild(collumnName);
+    componentRoot.appendChild(collumnContent);
+
+    return componentRoot;
+  }
+
+  style() {
+    const style = document.createElement("style");
+    style.textContent = `
+    * {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      box-sizing: border-box;
+    }
+    
+    .tasksCollumn {
+      background-color: #ebecf0;
+      border-radius: 3px;
+      padding: 10px;
+      width: 30vw;
+      min-width: 250px;
+      max-width: 350px;
+      margin-right: 5px;
+      margin-left: 5px;
+      max-height: 450px;
+      overflow-y: auto;
+    }
+    
+    @media screen and (max-width: 700px) {      
+      .tasksCollumn {
+        min-width: 350px;
+        margin-bottom: 10px;
+      }
+    }
+
+    .tasksCollumn::-webkit-scrollbar {
+      width: 0px;
+    }
+    
+    .collumnName {
+      font-size: 18px;
+      font-weight: 500;
+      margin-bottom: 10px;
+    }
+    `;
+
+    return style;
+  }
+}
+
+customElements.define("task-collumn", TaskCollumn);
