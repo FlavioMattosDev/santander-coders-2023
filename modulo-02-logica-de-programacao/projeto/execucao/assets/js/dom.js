9ff8c2e71a5c28b72dca800ae3862c5d4a1c9479
Anderson Junior <acfaj2010@gmail.com>
2023-08-18 23:13:10 -0300

    Corrigido o fechamento do registerTaskModal

--- modulo-02-logica-de-programacao/projeto/execucao/assets/js/dom.js
+++ modulo-02-logica-de-programacao/projeto/execucao/assets/js/dom.js
@@ -1,32 +1,24 @@
 const addTaskButton = document.querySelector("button.add-task_button");
-const registerButtonModal = document.querySelector("button.register-task_button");
+const registerButtonModal = document.querySelector(
+  "button.register-task_button"
+);
 const registerTaskModal = document.querySelector("div.registerTaskModal");
+const registerTask = document.querySelector("div.registerTask");
 
-
-const clickInside = document.querySelector("div.registerTask")
-
+let isModalOpen = false;
 const toggleModal = (e) => {
-  e.stopPropagation()
+  e.stopPropagation();
 
-  if(e.target === clickInside){
-    return
-  }
+  isModalOpen = !isModalOpen;
+  isModalOpen
+    ? (registerTaskModal.style.display = "flex")
+    : (registerTaskModal.style.display = "none");
 
-  const isModalOpen = registerTaskModal.classList.contains("flex");
-
-  if(isModalOpen &&  !(e.target === registerTaskModal)){
-    registerTaskModal.classList.remove(isModalOpen ? "flex" : "hidden");
-    registerTaskModal.classList.add(isModalOpen ? "hidden" : "flex");
-
-    return
-  }
-
-  registerTaskModal.classList.remove(isModalOpen ? "flex" : "hidden");
-  registerTaskModal.classList.add(isModalOpen ? "hidden" : "flex");
+  registerTask.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 };
 
 addTaskButton.addEventListener("click", toggleModal);
 registerButtonModal.addEventListener("click", toggleModal);
 registerTaskModal.addEventListener("click", toggleModal);
-
-// const teste = document.querySelectorAll("individual-task");
