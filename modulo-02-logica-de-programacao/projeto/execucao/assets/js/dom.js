50867ddb427a572a702b8115b7d05678539761b0
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-20 16:16:36 -0300

    feat: implement modal funcionalities.

--- modulo-02-logica-de-programacao/projeto/execucao/assets/js/dom.js
+++ modulo-02-logica-de-programacao/projeto/execucao/assets/js/dom.js
@@ -1,25 +1,14 @@
 const addTaskButton = document.querySelector("button.add-task_button");
-const registerButtonModal = document.querySelector("button.register-task_button");
+const registerButtonModal = document.querySelector(
+  "button.register-task_button"
+);
 const registerTaskModal = document.querySelector("div.registerTaskModal");
-
-
-const clickInside = document.querySelector("div.registerTask")
+const registerTaskOverlay = document.querySelector(
+  "div.register-modal_overlay"
+);
 
 const toggleModal = (e) => {
-  e.stopPropagation()
-
-  if(e.target === clickInside){
-    return
-  }
-
   const isModalOpen = registerTaskModal.classList.contains("flex");
-
-  if(isModalOpen &&  !(e.target === registerTaskModal)){
-    registerTaskModal.classList.remove(isModalOpen ? "flex" : "hidden");
-    registerTaskModal.classList.add(isModalOpen ? "hidden" : "flex");
-
-    return
-  }
 
   registerTaskModal.classList.remove(isModalOpen ? "flex" : "hidden");
   registerTaskModal.classList.add(isModalOpen ? "hidden" : "flex");
@@ -27,6 +16,15 @@ const toggleModal = (e) => {
 
 addTaskButton.addEventListener("click", toggleModal);
 registerButtonModal.addEventListener("click", toggleModal);
-registerTaskModal.addEventListener("click", toggleModal);
+registerTaskOverlay.addEventListener("click", toggleModal);
 
 // const teste = document.querySelectorAll("individual-task");
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  const isModalOpen = registerTaskModal.classList.contains("flex");
+
+  if (e.key.toLowerCase() === "escape" && isModalOpen) {
+    toggleModal();
+  }
+});
