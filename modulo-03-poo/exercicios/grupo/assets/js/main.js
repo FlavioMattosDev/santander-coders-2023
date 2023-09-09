7cc047c88f83775ba7a04cd2310e35e9902cad76
Anderson Junior <acfaj2010@gmail.com>
2023-09-09 19:17:20 -0300

    adicionado status de jogador, controles, teclado virtual e botoes de ação no html

--- modulo-03-poo/exercicios/grupo/assets/js/main.js
+++ modulo-03-poo/exercicios/grupo/assets/js/main.js
@@ -1,8 +1,8 @@
 import { Map } from "./Map.js";
 import { Player } from "./Player.js";
 
-const player = new Player()
+const player = new Player();
 
-console.log(Map.isPositionValid({y: 25 , x: 15}))
+console.log(Map.isPositionValid({ y: 25, x: 15 }));
 
-player.init()
+player.init();
