f68eee50f8b2a0b2035d30f3ee26c33cead0af92
georgrybski <rybskigeorg@gmail.com>
2023-09-13 22:01:10 -0300

    feat(Chest): Added open chest functionality

--- modulo-03-poo/exercicios/grupo/assets/js/Chest.js
+++ modulo-03-poo/exercicios/grupo/assets/js/Chest.js
@@ -5,12 +5,12 @@ import {Map} from "./Map.js";
 
 export class Chest extends Entity {
     #treasure;
-    #open;
+    #isOpen;
 
     constructor(x, y) {
         super()
         this.#treasure = (Math.random() <  Map.difficult.positiveLootOutcomePercentChance / 100)? new Blessing() : new Curse();
-        this.#open = false;
+        this.#isOpen = false;
 
         if (Map.isPositionValid({ x, y })) {
             this.xActualPosition = x;
@@ -22,21 +22,73 @@ export class Chest extends Entity {
         Map.addEntityToMappedEntities(this);
         this.render()
     }
+    get isOpen() {
+        return this.#isOpen;
+    }
+
+    #openChest() {
+        console.log("Context in #openChest: ", this);
+        this.#isOpen = true;
+        this.#updateImage();
+    }
+
     give(player) {
+        if (!this.#treasure) {
+            return;
+        }
+        this.#openChest();
         this.#treasure.give(player);
-        this.#open = true;
+        this.#treasure = null;
     }
 
-    render(){
-        const screenMap = document.querySelector('#map')
-        const chest = document.createElement('div')
-        const chestImage = document.createElement('img')
-        chestImage.src = `../grupo/assets/images/icons/chest.png`
-        chestImage.className = "absolute top-0 left-0"
+    // static #updateImage(chest) {
+    //     chest.#isOpen ?
+    //         (chest) => {
+    //
+    //     } : (chest) => {
+    //
+    //     }
+    // }
 
-        chest.className = `absolute top-[calc(${this.yActualPosition}*20px)] left-[calc(${this.xActualPosition}*20px)] w-5 h-5 overflow-hidden`
-        chest.appendChild(chestImage)
+    // render(){
+    //     const screenMap = document.querySelector('#map')
+    //     const chest = document.createElement('div')
+    //     const chestImage = document.createElement('img')
+    //     chestImage.src = `../grupo/assets/images/icons/chest.png`
+    //     chestImage.className = "absolute top-0 left-0"
+    //
+    //     chest.className = `absolute top-[calc(${this.yActualPosition}*20px)] left-[calc(${this.xActualPosition}*20px)] w-5 h-5 overflow-hidden`
+    //     chest.appendChild(chestImage)
+    //
+    //     screenMap.appendChild(chest)
+    // }
+    #updateImage() {
+        let imageClasses = [...this.chestImageElement.classList].filter(c => !c.startsWith('top-'));
 
-        screenMap.appendChild(chest)
+        if (this.#isOpen) {
+            imageClasses.push(`bottom-1`);
+        } else {
+            imageClasses.push(`top-0`);
+        }
+        this.chestImageElement.className = '';
+
+        this.chestImageElement.classList.add(...imageClasses);
     }
+
+    render() {
+        const screenMap = document.querySelector('#map');
+        this.chestElement = document.createElement('div');
+        this.chestImageElement = document.createElement('img');
+
+        this.chestImageElement.src = `../grupo/assets/images/icons/chest.png`;
+        this.chestImageElement.className = "absolute top-0 left-0";
+
+        this.chestElement.className = `absolute top-[calc(${this.yActualPosition}*20px)] left-[calc(${this.xActualPosition}*20px)] w-5 h-5 overflow-hidden`;
+        this.chestElement.appendChild(this.chestImageElement);
+
+        screenMap.appendChild(this.chestElement);
+    }
+
+
+
 }
